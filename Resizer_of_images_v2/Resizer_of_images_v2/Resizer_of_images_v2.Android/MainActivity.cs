14a059399e6462c88875e86a1e1827a0388f5756
KSKamburova18 <KSKamburova18@codingburgas.bg>
2021-07-13 12:13:25 +0300

    Added a way to go to downloaded pictures in the emulator. Resize functionality to be add

--- Resizer_of_images_v2/Resizer_of_images_v2/Resizer_of_images_v2.Android/MainActivity.cs
+++ Resizer_of_images_v2/Resizer_of_images_v2/Resizer_of_images_v2.Android/MainActivity.cs
@@ -24,43 +24,43 @@ namespace Resizer_of_images_v2.Droid
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-        public byte[] ResizeImage(byte[] imageData, float width, float height)
-        {
+        //public byte[] ResizeImage(byte[] imageData, float width, float height)
+        //{
 
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.InPurgeable = true;
-            Bitmap originalImage = BitmapFactory.DecodeByteArray(imageData, 0, imageData.Length, options);
+        //    BitmapFactory.Options options = new BitmapFactory.Options();
+        //    options.InPurgeable = true;
+        //    Bitmap originalImage = BitmapFactory.DecodeByteArray(imageData, 0, imageData.Length, options);
 
-            float newHeight = 0;
-            float newWidth = 0;
+        //    float newHeight = 0;
+        //    float newWidth = 0;
 
-            var originalHeight = originalImage.Height;
-            var originalWidth = originalImage.Width;
+        //    var originalHeight = originalImage.Height;
+        //    var originalWidth = originalImage.Width;
 
-            if (originalHeight > originalWidth)
-            {
-                newHeight = height;
-                float ratio = originalHeight / height;
-                newWidth = originalWidth / ratio;
-            }
-            else
-            {
-                newWidth = width;
-                float ratio = originalWidth / width;
-                newHeight = originalHeight / ratio;
-            }
+        //    if (originalHeight > originalWidth)
+        //    {
+        //        newHeight = height;
+        //        float ratio = originalHeight / height;
+        //        newWidth = originalWidth / ratio;
+        //    }
+        //    else
+        //    {
+        //        newWidth = width;
+        //        float ratio = originalWidth / width;
+        //        newHeight = originalHeight / ratio;
+        //    }
 
-            Bitmap resizedImage = Bitmap.CreateScaledBitmap(originalImage, (int)newWidth, (int)newHeight, true);
+        //    Bitmap resizedImage = Bitmap.CreateScaledBitmap(originalImage, (int)newWidth, (int)newHeight, true);
 
-            originalImage.Recycle();
+        //    originalImage.Recycle();
 
-            using (MemoryStream ms = new MemoryStream())
-            {
-                resizedImage.Compress(Bitmap.CompressFormat.Png, 100, ms);
+        //    using (MemoryStream ms = new MemoryStream())
+        //    {
+        //        resizedImage.Compress(Bitmap.CompressFormat.Png, 100, ms);
 
-                resizedImage.Recycle();
+        //        resizedImage.Recycle();
 
-                return ms.ToArray();
-            }
+        //        return ms.ToArray();
+        //    }
         }
 }
--- Resizer_of_images_v2/Resizer_of_images_v2/Resizer_of_images_v2/Views/ItemsPage.xaml.cs
+++ Resizer_of_images_v2/Resizer_of_images_v2/Resizer_of_images_v2/Views/ItemsPage.xaml.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace Resizer_of_images_v2.Views
 {
@@ -30,6 +31,21 @@ namespace Resizer_of_images_v2.Views
             _viewModel.OnAppearing();
         }
 
-        
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var file = await FilePicker.PickAsync();
+
+                if (file == null)
+                    return;
+
+               // Content.FindByName("Something") = file.FileName;
+            }
+            catch (Exception)
+            { 
+            }
+        }
+
     }
 }
